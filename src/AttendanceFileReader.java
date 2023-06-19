ddfb1b9c04611eac3c7b1c738e2b79dba2539d98
CSMMDC <sydneychuawas@gmail.com>
2023-06-19 15:20:13 +0800

    Included GUIEUROPA.GPP
    .GPP files are for GUIGenie

--- src/AttendanceFileReader.java
+++ src/AttendanceFileReader.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 
-public class AttendanceFileReader(String inputmo) {
+public class AttendanceFileReader(String inputnum, String inputmo) {
 
     public static void AttenFileRead(){
         ArrayList<Attendance> employeeAttendances = new ArrayList<Attendance>();
